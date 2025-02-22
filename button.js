647315b9e1799c816de41328a94f39ccf006863c
emaan-mir <thisisemaanmeer@gmail.com>
2025-02-22 19:50:06 +0500

    Add files via upload

---
+++ button.js
@@ -0,0 +1,32 @@
+const button = document.getElementById('search-button');
+const input = document.getElementById('city-input');
+
+const cityName =document.getElementById('city-name');
+const cityTime =document.getElementById('city-time');
+const cityTemp =document.getElementById('city-temp');
+
+
+
+
+ async  function getdata(cityName)
+{
+
+const promise= await fetch (
+    `http://api.weatherapi.com/v1/current.json?key=ccfe8043b25f4d1695d91052252202&q=${cityName}&aqi=yes`);
+
+return await promise.json()
+
+}
+
+
+button.addEventListener("click", async () => {
+
+const value = input.value;
+const result= await getdata(value);
+ cityName.innerText =`${result.location.name},${result.location.region}-${result.location.country}`
+ cityTime.innerText = result.location.localtime;
+ cityTemp.innerText = result.current.temp_c;
+
+} 
+
+);
